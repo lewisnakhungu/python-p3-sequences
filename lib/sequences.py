516158af4559ecf44f4e1ab47a0e19974db6e3bc
LewisRobertNakhungu <lokiyule@gmail.com>
2025-05-08 21:07:09 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+   if length == 0:
+      print ([])
+      return 
+
+   sequence = [0]
+   if length == 1:
+      print (sequence)
+      return 
+      
+   
+   sequence.append(1)
+   for i in range (2, length):
+      next_value = sequence[-1] + sequence[-2]
+      sequence.append(next_value)
+
+   print (sequence)
